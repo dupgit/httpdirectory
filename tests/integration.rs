143459a8fad261cbb402330f9a6aef8cd2c11f3a
Olivier Delhomme <olivier.delhomme@free.fr>
2025-05-16 21:54:01 +0200

    fix(tests): Adds a new test http directory
    This test directory has size and date fields reversed and date format
    is the legacy apache 2.2 one. We are not testing all entries but only
    a few ones

--- tests/integration.rs
+++ tests/integration.rs
@@ -325,3 +325,138 @@ async fn test_bsd_example() {
 
     mock.assert();
 }
+
+// Tests <pre> tag with other formatted columns
+#[tokio::test]
+async fn test_old_bsd_example() {
+    // Start a lightweight mock server.
+    let server = MockServer::start();
+    let url = server.url("/old-bsd");
+
+    let mock = server.mock(|when, then| {
+        when.path("/old-bsd");
+        then.status(200).body(r##"
+            <!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width"><style type="text/css">body,html {background:#fff;font-family:"Bitstream Vera Sans","Lucida Grande","Lucida Sans Unicode",Lucidux,Verdana,Lucida,sans-serif;}tr:nth-child(even) {background:#f4f4f4;}th,td {padding:0.1em 0.5em;}th {text-align:left;font-weight:bold;background:#eee;border-bottom:1px solid #aaa;}#list {border:1px solid #aaa;width:100%;}a {color:#a33;}a:hover {color:#e33;}</style>
+
+            <title>Index of /pub/OpenBSD/</title>
+            </head><body><h1>Index of /pub/OpenBSD/</h1>
+            <table id="list"><thead><tr><th style="width:55%"><a href="?C=N&amp;O=A">File Name</a>&nbsp;<a href="?C=N&amp;O=D">&nbsp;&darr;&nbsp;</a></th><th style="width:20%"><a href="?C=S&amp;O=A">File Size</a>&nbsp;<a href="?C=S&amp;O=D">&nbsp;&darr;&nbsp;</a></th><th style="width:25%"><a href="?C=M&amp;O=A">Date</a>&nbsp;<a href="?C=M&amp;O=D">&nbsp;&darr;&nbsp;</a></th></tr></thead>
+            <tbody><tr><td class="link"><a href="../">Parent directory/</a></td><td class="size">-</td><td class="date">-</td></tr>
+            <tr><td class="link"><a href="2.0/" title="2.0">2.0/</a></td><td class="size">-</td><td class="date">2001-Jun-04 14:06</td></tr>
+            <tr><td class="link"><a href="2.1/" title="2.1">2.1/</a></td><td class="size">-</td><td class="date">2001-Jun-04 15:20</td></tr>
+            <tr><td class="link"><a href="2.2/" title="2.2">2.2/</a></td><td class="size">-</td><td class="date">2018-Feb-09 09:55</td></tr>
+            <tr><td class="link"><a href="2.3/" title="2.3">2.3/</a></td><td class="size">-</td><td class="date">2001-Jun-04 15:42</td></tr>
+            <tr><td class="link"><a href="2.4/" title="2.4">2.4/</a></td><td class="size">-</td><td class="date">2001-Jun-04 15:51</td></tr>
+            <tr><td class="link"><a href="2.5/" title="2.5">2.5/</a></td><td class="size">-</td><td class="date">2000-Jul-08 02:57</td></tr>
+            <tr><td class="link"><a href="2.6/" title="2.6">2.6/</a></td><td class="size">-</td><td class="date">2001-Jun-07 00:50</td></tr>
+            <tr><td class="link"><a href="2.7/" title="2.7">2.7/</a></td><td class="size">-</td><td class="date">2003-Oct-24 19:50</td></tr>
+            <tr><td class="link"><a href="2.8/" title="2.8">2.8/</a></td><td class="size">-</td><td class="date">2003-Oct-24 21:17</td></tr>
+            <tr><td class="link"><a href="2.9/" title="2.9">2.9/</a></td><td class="size">-</td><td class="date">2002-Oct-30 14:41</td></tr>
+            <tr><td class="link"><a href="3.0/" title="3.0">3.0/</a></td><td class="size">-</td><td class="date">2005-Oct-20 19:58</td></tr>
+            <tr><td class="link"><a href="3.1/" title="3.1">3.1/</a></td><td class="size">-</td><td class="date">2005-Oct-20 20:00</td></tr>
+            <tr><td class="link"><a href="3.2/" title="3.2">3.2/</a></td><td class="size">-</td><td class="date">2005-Oct-20 20:01</td></tr>
+            <tr><td class="link"><a href="3.3/" title="3.3">3.3/</a></td><td class="size">-</td><td class="date">2004-Nov-06 17:21</td></tr>
+            <tr><td class="link"><a href="3.4/" title="3.4">3.4/</a></td><td class="size">-</td><td class="date">2004-Nov-06 17:15</td></tr>
+            <tr><td class="link"><a href="3.5/" title="3.5">3.5/</a></td><td class="size">-</td><td class="date">2005-Oct-20 20:03</td></tr>
+            <tr><td class="link"><a href="3.6/" title="3.6">3.6/</a></td><td class="size">-</td><td class="date">2004-Nov-12 18:36</td></tr>
+            <tr><td class="link"><a href="3.7/" title="3.7">3.7/</a></td><td class="size">-</td><td class="date">2005-Apr-01 05:17</td></tr>
+            <tr><td class="link"><a href="3.8/" title="3.8">3.8/</a></td><td class="size">-</td><td class="date">2005-Sep-25 07:30</td></tr>
+            <tr><td class="link"><a href="3.9/" title="3.9">3.9/</a></td><td class="size">-</td><td class="date">2006-May-01 10:47</td></tr>
+            <tr><td class="link"><a href="4.0/" title="4.0">4.0/</a></td><td class="size">-</td><td class="date">2006-Dec-10 20:53</td></tr>
+            <tr><td class="link"><a href="4.1/" title="4.1">4.1/</a></td><td class="size">-</td><td class="date">2007-Apr-30 17:15</td></tr>
+            <tr><td class="link"><a href="4.2/" title="4.2">4.2/</a></td><td class="size">-</td><td class="date">2007-Oct-31 22:10</td></tr>
+            <tr><td class="link"><a href="4.3/" title="4.3">4.3/</a></td><td class="size">-</td><td class="date">2008-Apr-30 18:58</td></tr>
+            <tr><td class="link"><a href="4.4/" title="4.4">4.4/</a></td><td class="size">-</td><td class="date">2008-Sep-04 22:43</td></tr>
+            <tr><td class="link"><a href="4.5/" title="4.5">4.5/</a></td><td class="size">-</td><td class="date">2009-Mar-25 16:36</td></tr>
+            <tr><td class="link"><a href="4.6/" title="4.6">4.6/</a></td><td class="size">-</td><td class="date">2009-Oct-09 00:26</td></tr>
+            <tr><td class="link"><a href="4.7/" title="4.7">4.7/</a></td><td class="size">-</td><td class="date">2010-Apr-04 22:23</td></tr>
+            <tr><td class="link"><a href="4.8/" title="4.8">4.8/</a></td><td class="size">-</td><td class="date">2010-Nov-01 16:22</td></tr>
+            <tr><td class="link"><a href="4.9/" title="4.9">4.9/</a></td><td class="size">-</td><td class="date">2011-Apr-28 06:01</td></tr>
+            <tr><td class="link"><a href="5.0/" title="5.0">5.0/</a></td><td class="size">-</td><td class="date">2011-Oct-31 02:31</td></tr>
+            <tr><td class="link"><a href="5.1/" title="5.1">5.1/</a></td><td class="size">-</td><td class="date">2012-May-02 00:20</td></tr>
+            <tr><td class="link"><a href="5.2/" title="5.2">5.2/</a></td><td class="size">-</td><td class="date">2013-Mar-05 11:07</td></tr>
+            <tr><td class="link"><a href="5.3/" title="5.3">5.3/</a></td><td class="size">-</td><td class="date">2013-Jul-25 14:25</td></tr>
+            <tr><td class="link"><a href="5.4/" title="5.4">5.4/</a></td><td class="size">-</td><td class="date">2014-Mar-02 11:06</td></tr>
+            <tr><td class="link"><a href="5.5/" title="5.5">5.5/</a></td><td class="size">-</td><td class="date">2014-May-01 05:01</td></tr>
+            <tr><td class="link"><a href="5.6/" title="5.6">5.6/</a></td><td class="size">-</td><td class="date">2014-Nov-01 14:09</td></tr>
+            <tr><td class="link"><a href="5.7/" title="5.7">5.7/</a></td><td class="size">-</td><td class="date">2015-May-14 10:48</td></tr>
+            <tr><td class="link"><a href="5.8/" title="5.8">5.8/</a></td><td class="size">-</td><td class="date">2015-Oct-18 14:54</td></tr>
+            <tr><td class="link"><a href="5.9/" title="5.9">5.9/</a></td><td class="size">-</td><td class="date">2016-Mar-27 23:01</td></tr>
+            <tr><td class="link"><a href="6.0/" title="6.0">6.0/</a></td><td class="size">-</td><td class="date">2016-Aug-23 22:23</td></tr>
+            <tr><td class="link"><a href="6.1/" title="6.1">6.1/</a></td><td class="size">-</td><td class="date">2017-Apr-11 22:57</td></tr>
+            <tr><td class="link"><a href="6.2/" title="6.2">6.2/</a></td><td class="size">-</td><td class="date">2017-Oct-11 10:17</td></tr>
+            <tr><td class="link"><a href="6.3/" title="6.3">6.3/</a></td><td class="size">-</td><td class="date">2018-Apr-07 00:17</td></tr>
+            <tr><td class="link"><a href="6.4/" title="6.4">6.4/</a></td><td class="size">-</td><td class="date">2018-Oct-29 16:29</td></tr>
+            <tr><td class="link"><a href="6.5/" title="6.5">6.5/</a></td><td class="size">-</td><td class="date">2019-Aug-11 14:18</td></tr>
+            <tr><td class="link"><a href="6.6/" title="6.6">6.6/</a></td><td class="size">-</td><td class="date">2019-Oct-23 16:54</td></tr>
+            <tr><td class="link"><a href="6.7/" title="6.7">6.7/</a></td><td class="size">-</td><td class="date">2020-May-18 14:55</td></tr>
+            <tr><td class="link"><a href="6.8/" title="6.8">6.8/</a></td><td class="size">-</td><td class="date">2021-Feb-08 01:58</td></tr>
+            <tr><td class="link"><a href="6.9/" title="6.9">6.9/</a></td><td class="size">-</td><td class="date">2021-Apr-27 13:54</td></tr>
+            <tr><td class="link"><a href="7.0/" title="7.0">7.0/</a></td><td class="size">-</td><td class="date">2021-Oct-15 02:55</td></tr>
+            <tr><td class="link"><a href="7.1/" title="7.1">7.1/</a></td><td class="size">-</td><td class="date">2022-Apr-21 13:27</td></tr>
+            <tr><td class="link"><a href="7.2/" title="7.2">7.2/</a></td><td class="size">-</td><td class="date">2022-Oct-18 06:11</td></tr>
+            <tr><td class="link"><a href="7.3/" title="7.3">7.3/</a></td><td class="size">-</td><td class="date">2023-Apr-08 01:02</td></tr>
+            <tr><td class="link"><a href="7.4/" title="7.4">7.4/</a></td><td class="size">-</td><td class="date">2023-Oct-16 15:44</td></tr>
+            <tr><td class="link"><a href="7.5/" title="7.5">7.5/</a></td><td class="size">-</td><td class="date">2024-Apr-05 11:59</td></tr>
+            <tr><td class="link"><a href="7.6/" title="7.6">7.6/</a></td><td class="size">-</td><td class="date">2024-Oct-08 17:17</td></tr>
+            <tr><td class="link"><a href="7.7/" title="7.7">7.7/</a></td><td class="size">-</td><td class="date">2025-Apr-27 17:58</td></tr>
+            <tr><td class="link"><a href="Changelogs/" title="Changelogs">Changelogs/</a></td><td class="size">-</td><td class="date">2025-May-16 07:33</td></tr>
+            <tr><td class="link"><a href="LibreSSL/" title="LibreSSL">LibreSSL/</a></td><td class="size">-</td><td class="date">2025-Apr-30 07:35</td></tr>
+            <tr><td class="link"><a href="OpenBGPD/" title="OpenBGPD">OpenBGPD/</a></td><td class="size">-</td><td class="date">2025-Feb-06 16:49</td></tr>
+            <tr><td class="link"><a href="OpenIKED/" title="OpenIKED">OpenIKED/</a></td><td class="size">-</td><td class="date">2025-Apr-10 18:11</td></tr>
+            <tr><td class="link"><a href="OpenNTPD/" title="OpenNTPD">OpenNTPD/</a></td><td class="size">-</td><td class="date">2020-Dec-09 14:56</td></tr>
+            <tr><td class="link"><a href="OpenSSH/" title="OpenSSH">OpenSSH/</a></td><td class="size">-</td><td class="date">2025-Apr-09 07:54</td></tr>
+            <tr><td class="link"><a href="doc/" title="doc">doc/</a></td><td class="size">-</td><td class="date">2013-Apr-28 15:57</td></tr>
+            <tr><td class="link"><a href="patches/" title="patches">patches/</a></td><td class="size">-</td><td class="date">2025-May-16 14:07</td></tr>
+            <tr><td class="link"><a href="rpki-client/" title="rpki-client">rpki-client/</a></td><td class="size">-</td><td class="date">2025-Apr-11 22:25</td></tr>
+            <tr><td class="link"><a href="snapshots/" title="snapshots">snapshots/</a></td><td class="size">-</td><td class="date">2023-Mar-26 14:18</td></tr>
+            <tr><td class="link"><a href="songs/" title="songs">songs/</a></td><td class="size">-</td><td class="date">2023-Apr-06 22:15</td></tr>
+            <tr><td class="link"><a href="stable/" title="stable">stable/</a></td><td class="size">-</td><td class="date">2022-Jan-18 16:25</td></tr>
+            <tr><td class="link"><a href="syspatch/" title="syspatch">syspatch/</a></td><td class="size">-</td><td class="date">2025-Mar-03 16:19</td></tr>
+            <tr><td class="link"><a href="tools/" title="tools">tools/</a></td><td class="size">-</td><td class="date">2005-Jan-07 19:40</td></tr>
+            <tr><td class="link"><a href="README" title="README">README</a></td><td class="size">               1249</td><td class="date">2021-May-25 20:15</td></tr>
+            <tr><td class="link"><a href="ftplist" title="ftplist">ftplist</a></td><td class="size">               4836</td><td class="date">2025-May-16 14:13</td></tr>
+            <tr><td class="link"><a href="timestamp" title="timestamp">timestamp</a></td><td class="size">                 11</td><td class="date">2025-May-16 14:13</td></tr>
+            </tbody></table><div>This server can also be reached on the Tor network at</div>
+            <div><a href="http://lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion/">lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion</a></div>
+            <div>Information:</div>
+            <div><a href="/datahanteringspolicy.txt">Data handling policy</a></div>
+            <div>The mirror administration can be reached at ftp-master (at) lysator.liu.se</div>
+        "##);
+    });
+
+    let httpdir = match HttpDirectory::new(&url).await {
+        Ok(httpdir) => httpdir,
+        Err(e) => panic!("{e}"),
+    };
+
+    // The library fails to get this directory properly
+    assert_eq!(httpdir.len(), 76);
+    let entries = httpdir.entries();
+
+    assert_entry(&entries[0], true, false, false, "../", 0, 0, 0, 0, 0, 0);
+    assert_entry(&entries[1], false, true, false, "2.0/", 0, 2001, 06, 04, 14, 06);
+    assert_entry(&entries[2], false, true, false, "2.1/", 0, 2001, 06, 04, 15, 20);
+    assert_entry(&entries[56], false, true, false, "7.5/", 0, 2024, 4, 05, 11, 59);
+    assert_entry(&entries[57], false, true, false, "7.6/", 0, 2024, 10, 08, 17, 17);
+    assert_entry(&entries[58], false, true, false, "7.7/", 0, 2025, 4, 27, 17, 58);
+    assert_entry(&entries[59], false, true, false, "Changelogs/", 0, 2025, 5, 16, 07, 33);
+    assert_entry(&entries[60], false, true, false, "LibreSSL/", 0, 2025, 04, 30, 07, 35);
+    assert_entry(&entries[61], false, true, false, "OpenBGPD/", 0, 2025, 02, 06, 16, 49);
+    assert_entry(&entries[62], false, true, false, "OpenIKED/", 0, 2025, 04, 10, 18, 11);
+    assert_entry(&entries[63], false, true, false, "OpenNTPD/", 0, 2020, 12, 09, 14, 56);
+    assert_entry(&entries[64], false, true, false, "OpenSSH/", 0, 2025, 04, 09, 07, 54);
+    assert_entry(&entries[65], false, true, false, "doc/", 0, 2013, 04, 28, 15, 57);
+    assert_entry(&entries[66], false, true, false, "patches/", 0, 2025, 05, 16, 14, 07);
+    assert_entry(&entries[67], false, true, false, "rpki-client/", 0, 2025, 04, 11, 22, 25);
+    assert_entry(&entries[68], false, true, false, "snapshots/", 0, 2023, 03, 26, 14, 18);
+    assert_entry(&entries[69], false, true, false, "songs/", 0, 2023, 04, 06, 22, 15);
+    assert_entry(&entries[70], false, true, false, "stable/", 0, 2022, 01, 18, 16, 25);
+    assert_entry(&entries[71], false, true, false, "syspatch/", 0, 2025, 03, 03, 16, 19);
+    assert_entry(&entries[72], false, true, false, "tools/", 0, 2005, 01, 07, 19, 40);
+    assert_entry(&entries[73], false, false, true, "README", 1249, 2021, 05, 25, 20, 15);
+    assert_entry(&entries[74], false, false, true, "ftplist", 4836, 2025, 05, 16, 14, 13);
+    assert_entry(&entries[75], false, false, true, "timestamp", 11, 2025, 05, 16, 14, 13);
+
+    mock.assert();
+}
