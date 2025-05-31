ac4067b4aecc62428275d1322eef5a6b0a6ddb23
Olivier Delhomme <olivier.delhomme@free.fr>
2025-05-31 23:05:32 +0200

    fix(examples): split onedir example and adds debug_me
    debug_me allows one to debug site by site those that need
    some analyze to get it right

---
+++ examples/debug_me.rs
@@ -0,0 +1,36 @@
+use env_logger::{Env, WriteStyle};
+use httpdirectory::httpdirectory::HttpDirectory;
+use std::env::var;
+
+#[tokio::main]
+async fn main() {
+    let no_color_compliance = match var("NO_COLOR").is_ok() {
+        true => WriteStyle::Never,
+        false => WriteStyle::Auto,
+    };
+
+    // Replace default_filter_or("") by default_filter_or("debug") to see debug message by default
+    // One may want to directly use RUST_LOG=httpdirectory=debug instead
+    env_logger::Builder::from_env(Env::default().default_filter_or("")).write_style(no_color_compliance).init();
+
+    let url_array = [
+        // "https://cloud.centos.org/centos/10-stream/x86_64/images/",
+        // "https://cloud-images.ubuntu.com/noble/20250430/",
+        // "https://mirrors.ircam.fr/pub/fedora/linux/releases",
+        // "https://cloud.debian.org/images/cloud/",
+        // "https://mirrors.ircam.fr/pub/",
+        // "https://ftp.lip6.fr/pub/OpenBSD/",
+        // "https://ftp.lysator.liu.se/pub/OpenBSD/",
+        // "http://mirror.nju.edu.cn/debian/",
+        // "http://debian-archive.trafficmanager.net/debian/",
+        "http://mirrors.coreix.net/debian/",
+        "http://mirror.cov.ukservers.com/debian/",
+    ];
+
+    for url in url_array {
+        match HttpDirectory::new(url).await {
+            Ok(httpdir) => println!("{httpdir}"),
+            Err(myerr) => println!("{myerr}"),
+        }
+    }
+}
--- examples/onedir.rs
+++ examples/onedir.rs
@@ -13,23 +13,6 @@ async fn main() {
     // One may want to directly use RUST_LOG=httpdirectory=debug instead
     env_logger::Builder::from_env(Env::default().default_filter_or("")).write_style(no_color_compliance).init();
 
-    let url_array = [
-        "https://cloud.centos.org/centos/10-stream/x86_64/images/",
-        "https://cloud-images.ubuntu.com/noble/20250430/",
-        "https://mirrors.ircam.fr/pub/fedora/linux/releases",
-        "https://cloud.debian.org/images/cloud/",
-        "https://mirrors.ircam.fr/pub/",
-        "https://ftp.lip6.fr/pub/OpenBSD/",
-        "https://ftp.lysator.liu.se/pub/OpenBSD/",
-    ];
-
-    for url in url_array {
-        match HttpDirectory::new(url).await {
-            Ok(httpdir) => println!("{httpdir}"),
-            Err(myerr) => println!("{myerr}"),
-        }
-    }
-
     if let Ok(httpdir) = HttpDirectory::new("https://cloud.debian.org/images/cloud/").await {
         match httpdir.dirs().filter_by_name("bookworm/") {
             Ok(httpdir) => {
