4f1a5d8f686032f3c7f56ac947b51d1ae4e99089
Olivier Delhomme <olivier.delhomme@free.fr>
2025-12-20 17:19:52 +0100

    style: be more rusty and splits into small functions

--- examples/mirrors.rs
+++ examples/mirrors.rs
@@ -1,10 +1,14 @@
 use colored::Colorize;
 use env_logger::{Env, WriteStyle};
 use httpdirectory::httpdirectory::HttpDirectory;
+use httpdirectory::stats::Stats;
 use std::env::var;
+use std::fmt::Display;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::sync::Arc;
+use tokio::sync::Semaphore;
 use tokio::task::JoinSet;
 
 // The output is wrapped in a Result to allow matching on errors.
@@ -17,67 +21,99 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-#[tokio::main]
-#[cfg_attr(feature = "hotpath", hotpath::main(percentiles = [99]), flavor = "current_thread")]
-async fn main() {
-    let no_color_compliance = match var("NO_COLOR").is_ok() {
-        true => WriteStyle::Never,
-        false => WriteStyle::Auto,
-    };
+// we know that the directory should contain at least
+// 7 files and directories (none should be 0) and that
+// the sum of files with a date and the files without a
+// date must be the same than the sum of files, parent
+// directory and directories
+fn is_debian_invalid(httpdir: &HttpDirectory, stats: &Stats) -> bool {
+    httpdir.len() < 7
+        || stats.files == 0
+        || stats.dirs == 0
+        || stats.files + stats.dirs + u32::from(stats.parent_dir) != stats.with_date + stats.without_date
+}
+
+fn no_color_compliance() -> WriteStyle {
+    if var("NO_COLOR").is_ok() {
+        WriteStyle::Never
+    } else {
+        WriteStyle::Auto
+    }
+}
+
+fn setup_logging_system() {
+    let no_color_compliance = no_color_compliance();
 
     // Replace default_filter_or("") by default_filter_or("debug") to see debug message by default
     // One may want to directly use RUST_LOG=httpdirectory=debug instead
     env_logger::Builder::from_env(Env::default().default_filter_or("")).write_style(no_color_compliance).init();
+}
 
-    let mut urls = vec![];
+/// Prints a structure T as a whole into green
+fn print_in_green<T>(to_print: &T)
+where
+    T: Display + ?Sized,
+{
+    println!("{}", to_print.to_string().green());
+}
 
-    if let Ok(lines) = read_lines("./mirror.list") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines.map_while(Result::ok) {
-            urls.push(line);
-        }
-    }
-    let capacity = urls.len();
-    println!("Total mirror sites: {capacity}");
+/// Prints a structure T as a whole into red
+fn print_in_red<T>(to_print: &T)
+where
+    T: Display + ?Sized,
+{
+    println!("{}", to_print.to_string().red());
+}
+
+#[tokio::main]
+#[cfg_attr(feature = "hotpath", hotpath::main(percentiles = [99]))]
+async fn main() {
+    setup_logging_system();
+
+    let urls: Vec<String> =
+        read_lines("./mirror.list").expect("Error reading file mirror.list").map_while(Result::ok).collect();
+    println!("{}", format!("Total mirror sites: {}", urls.len()).bright_blue().bold());
+
     let mut tasks = JoinSet::new();
 
     // spawning tasks
-    let _vec_abort_handle: Vec<_> =
-        urls.into_iter().map(|url| tasks.spawn(async move { HttpDirectory::new(&url).await })).collect();
+    // Limit to 16 tasks at a time
+    let semaphore = Arc::new(Semaphore::new(16));
+
+    for url in urls {
+        let semaphore = semaphore.clone();
+        tasks.spawn(async move {
+            let _permit = semaphore.acquire().await.expect("Semaphore closed prematurely");
+            HttpDirectory::new(&url).await
+        });
+    }
 
     let mut correct = 0;
     let mut errored = 0;
 
     while let Some(task) = tasks.join_next().await {
-        let result_httpdir = task.unwrap();
+        match task {
+            Ok(result_httpdir) => match result_httpdir {
+                Ok(httpdir) => {
+                    let stats = httpdir.stats();
 
-        match result_httpdir {
-            Ok(httpdir) => {
-                let stats = httpdir.stats();
-
-                // we know that the directory should contain at least
-                // 7 files and directories (none should be 0) and that
-                // every file and directory must have a date
-                if httpdir.len() < 7
-                    || stats.files == 0
-                    || stats.dirs == 0
-                    || stats.files + stats.dirs + stats.parent_dir as u32 != stats.with_date + stats.without_date
-                {
-                    println!("{}", "Strange result with this mirror:".red().bold());
-                    println!("{}", stats.to_string().red());
-
-                    println!("{}", httpdir.to_string().red());
-                    errored += 1;
-                } else {
-                    println!("{}", httpdir.to_string().green());
-                    correct += 1;
+                    if is_debian_invalid(&httpdir, &stats) {
+                        println!("{}", "Strange result with this mirror:".red().bold());
+                        print_in_red(&stats);
+                        print_in_red(&httpdir);
+                        errored += 1;
+                    } else {
+                        print_in_green(&httpdir);
+                        correct += 1;
+                    }
                 }
-            }
-            Err(myerr) => println!("{myerr}"),
+                Err(myerr) => println!("{myerr}"),
+            },
+            Err(e) => println!("task error: {e}"),
         }
     }
 
     println!();
-    println!("May be correct: {}", correct.to_string().green());
-    println!("Probably wrong: {}", errored.to_string().red());
+    print_in_green(&format!("May be correct: {correct}"));
+    print_in_red(&format!("Probably wrong: {errored}"));
 }
